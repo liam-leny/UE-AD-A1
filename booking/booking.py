56f6cff822995bc02df72aa3d30c8a487f3de782
liam-leny <liamleny@protonmail.com>
2024-09-30 21:22:49 +0200

    Development of Showtime and Booking services

--- booking/booking.py
+++ booking/booking.py
@@ -15,6 +15,77 @@ with open('{}/databases/bookings.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+@app.route("/bookings", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(bookings), 200)
+    return res
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_bookings_by_userid(userid):
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            res = make_response(jsonify(booking),200)
+            return res
+    return make_response(jsonify({"error":"User ID not found"}),400)
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking(userid):
+    req = request.get_json()
+    validity = False
+    userFound = False
+
+    if 'date' not in req or 'movieid' not in req:
+        return make_response(jsonify({"error":"Missing 'date' or 'movieid' in request"}), 400)
+    
+   # Check the validity of the booking by calling the external service showtimes
+    showTimesReponse = requests.get('http://localhost:3202/showmovies/' + req["date"]).json()
+    print(showTimesReponse)
+    if req["movieid"] in showTimesReponse["movies"] :
+         validity = True
+
+    if not validity :
+       return make_response(jsonify({"error":"The movie is not available on the requested date"}), 400)
+        
+    # Check if the user has an existing booking
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            userFound = True
+            
+            # Check if the user has already made a booking on the requested date
+            dateFound = False
+            for dateEntry in booking["dates"]:
+                if dateEntry["date"] == req["date"]:
+                    dateEntry["movies"].append(req["movieid"])
+                    dateFound = True
+            
+            # If the user has never booked on this date, create a new entry
+            if not dateFound:
+                booking["dates"].append({
+                    "date": req["date"],
+                    "movies": [req["movieid"]]
+                })
+
+    # If the user has never made a booking, create a new user entry
+    if not userFound:
+        new_booking = {
+            "userid": userid,
+            "dates": [
+                {
+                    "date": req["date"],
+                    "movies": [req["movieid"]]
+                }
+            ]
+        }
+        bookings.append(new_booking)
+
+    write(bookings)
+    res = make_response(jsonify({"message": "Booking added successfully"}), 200)
+    return res
+
+def write(bookings):
+    data = {"bookings": bookings}
+    with open('./databases/bookings.json', 'w') as f:
+        json.dump(data, f, indent=2)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,19 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(schedule), 200)
+    return res
+
+@app.route("/showmovies/<date>", methods=['GET'])
+def get_shedule_by_date(date):
+    for oneSchedule in schedule:
+        if str(oneSchedule["date"]) == str(date):
+            res = make_response(jsonify(oneSchedule),200)
+            return res
+    return make_response(jsonify({"error":"Date not found"}),400)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
