de98699a0f1403a22be1aa71cb0249d1b2b4583d
liam-leny <liamleny@protonmail.com>
2024-11-21 15:10:43 +0100

    Modification from python3 to python to start services

--- start_services.sh
+++ start_services.sh
@@ -2,19 +2,19 @@
 
 # Start the GraphQL service
 echo "Starting the GraphQL movie service..."
-python3 movie/movie.py &
+python movie/movie.py &
 
 # Start the REST service
 echo "Starting the REST user service..."
-python3 user/user.py &
+python user/user.py &
 
 # Start the first gRPC service (booking)
 echo "Starting the gRPC booking service..."
-python3 booking/booking.py &
+python booking/booking.py &
 
 # Start the second gRPC service (showtime)
 echo "Starting the gRPC showtime service..."
-python3 showtime/showtime.py &
+python showtime/showtime.py &
 
 wait
 
